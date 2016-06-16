72cdc4517b3ed4165d158d7e9181f2c1ec4d65c4
Andrew Heffernan <aheffernan@constantcontact.com>
2016-06-16 14:23:08 -0400

    Intial Commit

--- src/main/java/com/constantcontact/main.java
+++ src/main/java/com/constantcontact/main.java
@@ -1,17 +1,17 @@
 package com.constantcontact;
 
-import com.constantcontact.ConstantContactFactory;
-import com.constantcontact.components.emailcampaigns.EmailCampaignResponse;
+import com.constantcontact.components.contacts.Contact;
+import com.constantcontact.components.contacts.ContactList;
 import com.constantcontact.components.emailcampaigns.tracking.clicks.EmailCampaignTrackingClick;
-import com.constantcontact.components.emailcampaigns.tracking.opens.EmailCampaignTrackingOpen;
 import com.constantcontact.components.generic.response.ResultSet;
 import com.constantcontact.exceptions.service.ConstantContactServiceException;
 import com.constantcontact.services.contactlists.IContactListService;
-import com.constantcontact.services.emailcampaigns.IEmailCampaignService;
+import com.constantcontact.services.contacts.IContactService;
 import com.constantcontact.services.emailcampaigns.tracking.IEmailCampaignTrackingService;
 import com.constantcontact.util.RawApiRequestError;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
@@ -21,32 +21,57 @@ public class main {
 
 
 
-    public static void main(String[] args){
-        try {
-            ConstantContactFactory constantContact = new ConstantContactFactory("3bb60361-f328-4e1c-81f0-472191be0d9e","xaqkn2gwyvyyk4wrthzgxwaj");
+    public static void main(String[] args) {
 
-            IEmailCampaignTrackingService Tracking = constantContact.createEmailCampaignTrackingService();
+        for (int t = 0; t < 60; t++) {
 
-            ResultSet<EmailCampaignTrackingClick> Clicks = Tracking.getClicksByLinkId("1124977684869", "465668312939", 100, "2016-06-08");
+            try {
 
-            Clicks.getMeta();
-            List<EmailCampaignTrackingClick> Results = Clicks.getResults();
+                ConstantContactFactory constantContact = new ConstantContactFactory("3bb60361-f328-4e1c-81f0-472191be0d9e", "xaqkn2gwyvyyk4wrthzgxwaj");
 
-            String CLickedContact = Results.get(0).getEmailAddress();
+                //Get tracks for specific Campaigns
+                IEmailCampaignTrackingService Tracking = constantContact.createEmailCampaignTrackingService();
+                ResultSet<EmailCampaignTrackingClick> Clicks = Tracking.getClicksByLinkId("1124977684869", "465668312939", 100, "2016-06-08");
 
-            //ClickedContact
-        }
-        catch (ConstantContactServiceException e)
-        {
-            ConstantContactServiceException c = (ConstantContactServiceException)e.getCause();
-            for (Iterator<RawApiRequestError> i = c.getErrorInfo().iterator(); i.hasNext(); ) {
-                RawApiRequestError item = i.next();
-                System.out.println(item.getErrorKey() + " : " + item.getErrorMessage());
+                //set Contacts services
+                IContactListService Lists = constantContact.createContactListService();
+                List<ContactList> AddtoThisList = Lists.getLists("2016-01-08");
+                IContactService IcontactService = constantContact.createContactService();
+                List<EmailCampaignTrackingClick> Results = Clicks.getResults();
+
+                for (EmailCampaignTrackingClick e : Results) {
+
+                    String CLickedContact = e.getEmailAddress();
+                    ResultSet<Contact> ContactSet = IcontactService.getContactByEmail(CLickedContact);
+
+                    for (Contact x : ContactSet.getResults()) {
+
+                        x.setLists(AddtoThisList);
+
+                        IcontactService.updateContact(x, false);
+                    }
+
+                }
+
+            } catch (ConstantContactServiceException e) {
+                ConstantContactServiceException c = (ConstantContactServiceException) e.getCause();
+                for (Iterator<RawApiRequestError> i = c.getErrorInfo().iterator(); i.hasNext(); ) {
+                    RawApiRequestError item = i.next();
+                    System.out.println(item.getErrorKey() + " : " + item.getErrorMessage());
+                }
+                e.printStackTrace();
             }
-            e.printStackTrace();
+
+            try {
+                Thread.sleep(15000);
+
+                System.out.println("Sleeping");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
+
     }
-
-
 }
 
